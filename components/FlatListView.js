d292f27b4ce2a61e02c64443c1cf6d120eacafef
Serhii Romanenko <sergeyromanenko911@gmail.com>
2018-05-02 23:11:55 +0300

    add swipe and remove

--- components/FlatListView.js
+++ components/FlatListView.js
@@ -8,34 +8,87 @@ import {
     Image,
     ScrollView,
     Dimensions,
-    FlatList
+    FlatList,
+    Alert
         } from 'react-native';
+
+import Swipeout from 'react-native-swipeout';
 import FlatListData from '../data/FlatList';
 
 class ListItemCOmponent extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            activeRowKey:null
+        }
+    }
     render(){
+        const swipeSettings = {
+            autoClose:true,
+            onClose:(secId, rowId, direction)=>{
+                if(this.state.activeRowKey!==null){
+                    this.setState({
+                        activeRowKey:null
+                    });
+                }
+            },
+            onOpen:(secId, rowId, direction)=>{
+                this.setState({
+                    activeRowKey:this.props.item.key
+                });
+            },
+            right:[
+                {
+                    onPress:()=>{
+                        const deletingRow = this.state.activeRowKey;
+                        Alert.alert(
+                            'Alert',
+                            'Are u sure you want to delete?',
+                            [
+                                {text:'No', onPress:()=>console.log('Cancel Pressed'), style:'cancel'},
+                                {text:'Yes', onPress: ()=>{
+                                    FlatListData.splice(this.props.index, 1);
+                                    //Rfresh!
+                                    this.props.parentFlatList.refreshFlatList(deletingRow);
+                                }},
+                            ],
+                            {cancelable:true}
+                        );
+                    },
+                    text:'Delete', type:'delete'
+                }
+            ],
+            rowId:this.props.index,
+            sectionId:1
+
+        }
         return(
-            <View 
-                style={{
-                    flex:1,
-                    flexDirection:'row',
-                    backgroundColor: 'mediumseagreen',
-                    borderBottomColor:'white',
-                    borderBottomWidth:1
-                }}
+            <Swipeout
+                {...swipeSettings}
             >
-                <Image
-                    source={{uri:this.props.item.image}}
-                    style={{width:100,height:100, margin:5}}
-                />
-                <View
-                    style={{flex:1, flexDirection:'column'}}
+                <View 
+                    style={{
+                        flex:1,
+                        flexDirection:'row',
+                        backgroundColor: 'mediumseagreen',
+                        borderBottomColor:'white',
+                        borderBottomWidth:1
+                    }}
                 >
-                    <Text style={styles.item}>{this.props.item.name}</Text>
-                    <Text style={styles.item}>{this.props.item.title}</Text>
+                    <Image
+                        source={{uri:this.props.item.image}}
+                        style={{width:100,height:100, margin:5}}
+                    />
+                    <View
+                        style={{flex:1, flexDirection:'column'}}
+                    >
+                        <Text style={styles.item}>{this.props.item.name}</Text>
+                        <Text style={styles.item}>{this.props.item.title}</Text>
+                    </View>
+                    
                 </View>
-                
-            </View>
+            </Swipeout>
+            
         );
     }
 }
@@ -49,6 +102,19 @@ const styles = StyleSheet.create({
 })
 
 export default class FlatListView extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            deleteRowKey:null
+        }
+    }
+    refreshFlatList = (delKey) => {
+        this.setState((prevState)=>{
+            return {
+                deleteRowKey: delKey
+            }
+        })
+    }
     render(){
         return(
             <View style={{flex:1}}>
@@ -56,7 +122,7 @@ export default class FlatListView extends Component{
                     data={FlatListData}
                     renderItem={({item,index})=>{
                         return(
-                            <ListItemCOmponent item={item} index={index}/>
+                            <ListItemCOmponent item={item} index={index} parentFlatList = {this}/>
                         );
                     }}
                 ></FlatList>
--- data/FlatList.js
+++ data/FlatList.js
@@ -1,48 +1,48 @@
 var flatListData=[
    {
-       "key":"adasc12432c423c4141c314144",
+       "key":"adasc12432c423c4141c3141s44",
        "name":"Nina",
        "image":'https://cdn.shopify.com/s/files/1/1391/4483/products/owl-eyes-art-the-night_s-watch-zoom-detail_596052e3-4452-4acd-a7bf-a043831e244a_2048x.jpg',
        "title":"I am Nina"
    },
    {
-       "key":"adasc12432c423c4141c314144",
+       "key":"adasc12432c423c4141c314asdd144",
        "name":"Kolya",
        "image":'https://cdn.shopify.com/s/files/1/1391/4483/products/owl-eyes-art-the-night_s-watch-zoom-detail_596052e3-4452-4acd-a7bf-a043831e244a_2048x.jpg',
        "title":"I am Kolya"
    },
    {
-       "key":"adasc12432c423c4141c314144",
+       "key":"adasc12432c423c4141c31asdasdsad4144",
        "name":"Vasya",
        "image":'https://cdn.shopify.com/s/files/1/1391/4483/products/owl-eyes-art-the-night_s-watch-zoom-detail_596052e3-4452-4acd-a7bf-a043831e244a_2048x.jpg',
        "title":"I am Vasya"
    },
    {
-       "key":"adasc12432c423c4141c314144",
+       "key":"adasc12432c423c4141asdasdasdasdc314144",
        "name":"Nikita",
        "image":'https://cdn.shopify.com/s/files/1/1391/4483/products/owl-eyes-art-the-night_s-watch-zoom-detail_596052e3-4452-4acd-a7bf-a043831e244a_2048x.jpg',
        "title":"I am Nikita"
    },
    {
-       "key":"adasc12432c423c4141c314144",
+       "key":"adasc12432c423c4141c3asdsad14144",
        "name":"John",
        "image":'https://cdn.shopify.com/s/files/1/1391/4483/products/owl-eyes-art-the-night_s-watch-zoom-detail_596052e3-4452-4acd-a7bf-a043831e244a_2048x.jpg',
        "title":"I amJohna"
    },
    {
-       "key":"adasc12432c423c4141c314144",
+       "key":"adasc12432c423c4141c3asdas14144",
        "name":"Bob",
        "image":'https://cdn.shopify.com/s/files/1/1391/4483/products/owl-eyes-art-the-night_s-watch-zoom-detail_596052e3-4452-4acd-a7bf-a043831e244a_2048x.jpg',
        "title":"I am Bob"
    },
    {
-       "key":"adasc12432c423c4141c314144",
+       "key":"adasc12432c423c414asdasd1c314144",
        "name":"Jully",
        "image":'https://cdn.shopify.com/s/files/1/1391/4483/products/owl-eyes-art-the-night_s-watch-zoom-detail_596052e3-4452-4acd-a7bf-a043831e244a_2048x.jpg',
        "title":"I am Jully"
    },
    {
-       "key":"adasc12432c423c4141c314144",
+       "key":"adasc12432c423c41as41c314144",
        "name":"Anna",
        "image":'https://cdn.shopify.com/s/files/1/1391/4483/products/owl-eyes-art-the-night_s-watch-zoom-detail_596052e3-4452-4acd-a7bf-a043831e244a_2048x.jpg',
        "title":"I am Anna"
